09141335663206bea0a70e0ab831f20d7a058c1a
Brad Van Dyne <pyropyro78@gmail.com>
2019-01-29 16:32:46 -0800

    finished comments project

--- src/com/pyromanticgaming/cowsplosion/MainConfig.java
+++ src/com/pyromanticgaming/cowsplosion/MainConfig.java
@@ -8,14 +8,16 @@ public class MainConfig {
 	public MainConfig(cowSPLOSION instance) {
 		plugin = instance;
 	}
-
+	
+	//Set defaults
 	public static float ExplodePower = 1;
 	public static double alertdistance = 10;
 	public static boolean attackthecow, alertedbythecowtoggle;
 	public static String alertedbythecow;
 
 	public static void GetMainValues() {
-
+		
+		//Gather settings from config
 		ExplodePower = plugin.getConfig().getLong("ExplodePower");
 		alertdistance = plugin.getConfig().getDouble("AlertDistance");
 		attackthecow = plugin.getConfig().getBoolean("AttackTheCow");
